6fe87df8dcc196b7ada79efc08344fb95be20129
MikolajKos <niko.kosiorek@gmail.com>
2022-10-08 17:52:10 +0200

    Initial

---
+++ Exercises/Ex1 (WPF)/DataValidation/DataValidation/MVVM/Models/MainModel.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace DataValidation.MVVM.Models
+{
+    public class MainModel
+    {
+        public string firstName = "";
+        public string userAge = "";
+        
+        
+        public string firstNameRegex = @"^[a-zA-Z]*";
+        public string ageRegex = @"^[1-9]*";
+
+        
+
+        public List<string> ValidateInputData()
+        {
+            List<string> errorList = new List<string>();
+            errorList.Clear();
+
+            if(firstName == String.Empty || userAge == String.Empty)
+            {
+                if (firstName == String.Empty)
+                    errorList.Add("*Please enter your name.");
+                if (userAge == String.Empty)
+                    errorList.Add("*Please enter your age.");
+            }
+            else
+            {
+                if (!Regex.IsMatch(firstName, firstNameRegex))
+                    errorList.Add("*Name string is incorrect.");
+                if (!Regex.IsMatch(userAge, ageRegex))
+                    errorList.Add("*Age string is incorrect");
+
+                if (firstName.Length > 50)
+                    errorList.Add("Entered name is too long.");
+                if (userAge.Length > 50)
+                    errorList.Add("Entered age is too long.");
+
+            }
+
+            return errorList;
+        }
+    }
+}
---
+++ Exercises/Ex1 (WPF)/DataValidation/DataValidation/MVVM/ViewModels/MainViewModel.cs
@@ -0,0 +1,83 @@
+﻿using DataValidation.Core;
+using DataValidation.MVVM.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace DataValidation.MVVM.ViewModels
+{
+    public class MainViewModel : ObservableObject
+    {
+        MainModel model = new MainModel();
+
+        public string FirstNameTb
+        {
+            get
+            {
+                return model.firstName;
+            }
+            set
+            {
+                model.firstName = value;
+                onPropertyChanged(nameof(FirstNameTb));
+            }
+        }
+
+        public string AgeTb
+        {
+            get
+            {
+                return model.userAge;
+            }
+            set
+            {
+                model.userAge = value;
+                onPropertyChanged(nameof(AgeTb));
+            }
+        }
+
+        private List<string> _errorMessageTb;
+        public List<string> ErrorMessageTb 
+        {
+            get
+            {
+                return _errorMessageTb;
+            }
+            set
+            {
+                _errorMessageTb = value;
+                onPropertyChanged(nameof(ErrorMessageTb));
+            }
+        }
+
+
+
+        private ICommand _validateInput = null;
+
+        public ICommand ValidateInput
+        {
+            get
+            {
+                if (_validateInput == null) _validateInput = new RelayCommand(
+                    (object o) =>
+                    {
+                        ErrorMessageTb = model.ValidateInputData();
+                        
+                        onPropertyChanged(nameof(ValidateInput), nameof(FirstNameTb), nameof(AgeTb));
+                    },
+                    (object o) =>
+                    {
+/*                        if (FirstNameTb == String.Empty || AgeTb == String.Empty)
+                        {
+                            return false;
+                        }
+                        else*/ return true;
+                    });
+                return _validateInput;
+            }
+        }
+    }
+}
